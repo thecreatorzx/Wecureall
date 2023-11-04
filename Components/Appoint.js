decf9fd2e4b2770379b9b6c630fc2935885b2ede
AnshikaVerma38 <vermaanshika6821@gmail.com>
2023-11-04 13:34:05 +0530

    made some changes

--- Components/Appoint.js
+++ Components/Appoint.js
@@ -3,6 +3,7 @@ import React from "react";
 const Appoint = () => {
   return (
     <>
+    <p>Appointment will be made here</p>
       {/* You only need to work in between the <> and </> tags for your html part 
   and make your own css same as component name and import it in your component..  */}
     </>
