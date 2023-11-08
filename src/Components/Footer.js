0f03dd20873265fd0ff28cde05cdc15117c0e256
MohdSaad <saadyesican@gmail.com>
2023-11-08 21:28:23 +0530

    corrected the commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,70 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const Footer = () => {
   return (
     <>
       {/* You only need to work in between the <> and </> tags for your html part 
   and make your own css same as component name and import it in your component..  */}
 
-        <footer>
-
-          {/* Section 1 (footer) */}
-          <div className="sec sec1">
-            <div className="logo">logo</div>
-            <p>Medicines cure diseases but only doctors can cure patients.</p>
-            <h3 className="callCenter">Call Center:</h3>
-            <div className="num1">+054 043353</div>
-            <div className="num2">+034 832493</div>
-            <h3 className="follow">Follow Us On Social:</h3>
-            <div className="socialLinks">Social Links</div>
-          </div>
-
-
-          {/* Section 2(footer) */}
-          <div className="sec sec2">
-            <h3>About Us</h3>
-            <Link className="Services" to="/">
-                Services
-              </Link>
-            <Link className="Patients1" to="/">
-                For Patients 
-              </Link>
-            <Link className="Info" to="/">
-                Info 
-              </Link>
-            <Link className="Pricing" to="/">
-                Pricing
-              </Link>
-            <Link className="Contact" to="/">
-                Contact us
-              </Link>
+      <footer>
+        {/* Section 1 (footer) */}
+        <div className="sec sec1">
+          <div className="logo">logo</div>
+          <p>Medicines cure diseases but only doctors can cure patients.</p>
+          <h3 className="callCenter">Call Center:</h3>
+          <div className="num1">+054 043353</div>
+          <div className="num2">+034 832493</div>
+          <h3 className="follow">Follow Us On Social:</h3>
+          <div className="socialLinks">Social Links</div>
         </div>
 
-          {/* Section 3(footer) */}
+        {/* Section 2(footer) */}
+        <div className="sec sec2">
+          <h3>About Us</h3>
+          <Link className="Services" to="/">
+            Services
+          </Link>
+          <Link className="Patients1" to="/">
+            For Patients
+          </Link>
+          <Link className="Info" to="/">
+            Info
+          </Link>
+          <Link className="Pricing" to="/">
+            Pricing
+          </Link>
+          <Link className="Contact" to="/">
+            Contact us
+          </Link>
+        </div>
 
-          <div className="sec sec3">
-            <h3> For Patients</h3>
-            Links related to patients access of website
-          </div>
+        {/* Section 3(footer) */}
 
-          {/* Section 4(footer) */}
+        <div className="sec sec3">
+          <h3> For Patients</h3>
+          Links related to patients access of website
+        </div>
 
-          <div className="sec sec4">
-            <h3> For Hospitals</h3>
-            Section for 
-            <h3> FFor Doctors</h3>
-          </div>
+        {/* Section 4(footer) */}
 
-          {/* Section 5(footer) */}
+        <div className="sec sec4">
+          <h3> For Hospitals</h3>
+          Section for
+          <h3> For Doctors</h3>
+        </div>
 
-          <div className="sec sec4">
-            <h3>More</h3>
-          </div>
+        {/* Section 5(footer) */}
 
-        </footer>
-
+        <div className="sec sec4">
+          <h3>More</h3>
+        </div>
+      </footer>
     </>
   );
 };
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Nav.css";
 import { Outlet, Link } from "react-router-dom";
 import { useState, useRef } from "react";
-import { firebase } from "firebase/compat/app";
+import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/analytics";
@@ -24,50 +25,58 @@ const Nav = () => {
     <>
       {/* You only need to work in between the <> and </> tags for your html part 
   and make your own css same as component name and import it in your component..  */}
-      <nav>
-        <Link className="navLogo" to="/">
-          logo
+        <nav>
+      <div className="Start"><Link className = "navLogo" to="/">logo</Link>
+      </div>
+      <div className="Mid" >
+      <Link className="Services" to="/">
+          Services
         </Link>
-        <div className="Mid">
-          <Link className="Services" to="/">
-            Services
-          </Link>
-          <div className="Patients" onClick={expand()}>
-            <Link className="Patients1" to="/">
-              For Patients
-            </Link>
-            <Link className="Patients2" to="/">
-              For Patients2
-            </Link>
-            <Link className="Patients3" to="/">
-              For Patients3
-            </Link>
-          </div>
-          <div className="Info" onClick={expand}>
-            <Link className="Info" to="/">
-              Info1
-            </Link>
-            <Link className="Info2" to="/">
-              Info2
-            </Link>
-            <Link className="Info3" to="/">
-              Info3
-            </Link>
-          </div>
-          <Link className="Pricing" to="/">
-            Pricing
-          </Link>
-          <Link className="Contact" to="/">
-            Contact us
+      <div className="Patients" onClick={expand()}>
+      <Link className="Patients1" to="/">
+          For Patients &#10147;
+        </Link>
+      {/* <Link className="Patients2" to="/">
+          For Patients2
+        </Link>
+      <Link className="Patients3" to="/">
+          For Patients3
+        </Link> */}
+        </div>
+      <div className="Info" onClick={expand}>
+      <Link className="Info" to="/">
+          Info &#10147;
+        </Link>
+      {/* <Link className="Info2" to="/">
+          Info2
+        </Link>
+      <Link className="Info3" to="/">
+          Info3
+        </Link> */}
+      </div>
+      <Link className="Pricing" to="/">
+          Pricing
+        </Link>
+      <Link className="Contact" to="/">
+          Contact us
           </Link>
         </div>
 
-        <div className="sign">sign in and out</div>
+        <div className="sign">
+          <button className="logIn" onClick={logIn()}>
+            Log in
+          </button>
+          <button className="signIn" onClick={signIn()}>
+            Sign in
+          </button>
+        </div>
       </nav>
       <Outlet />
     </>
   );
   function expand() {}
+  function logIn() {}
+  function signIn() {}
 };
 
 export default Nav;
