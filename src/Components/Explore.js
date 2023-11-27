565f21d1fee635ae7cb5baa0025e5db4764b3a6b
Snehil38 <110400523+Snehil38@users.noreply.github.com>
2023-11-27 17:19:18 +0530

    only updating and adding required content

--- src/Components/Explore.js
+++ src/Components/Explore.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "./Explore.css";
 
 const Explore = () => {
   return (
     <>
-    <p>This is a new para</p>
+    <div class="exp_main">
+      <div class="exp_text">
+        <h2>Your future of quality<br/>
+        your Health</h2>
+        <p>Our team provides highly individualized medical
+          <br/>rehabilitation offering physical therapy, massage
+          <br/>therapy, icup, nurture and theopractic came to help
+          <br/>people of all ages and abilities to relef from<br/>
+          chronic pain</p>
+          <button class="button">
+            <span class="button-content">Explore More</span>
+          </button>
+      </div>
+      <div class="exp_img">
+        <img src="#"></img>
+      </div>
+    </div>
       {/* You only need to work in between the <> and </> tags for your html part 
   and make your own css same as component name and import it in your component..  */}
     </>
